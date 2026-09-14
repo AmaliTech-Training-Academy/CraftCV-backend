74de5336e04307711ba830e01755351e6c8811a6
Faith Etornam <faithgbadegbe1@gmail.com>
2026-09-14 11:56:46 +0000

    chore: folder-structure

--- craftcv/settings.py
+++ craftcv/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ydr-2h=dfafwn#f!z09vkd3^!e8+5&cag%p78!r7m@ha-n3qo3'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
