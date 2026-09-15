9a8ed2091237fccb18824df73c0ac0601b31e1d7
Faith Etornam <faithgbadegbe1@gmail.com>
2026-09-15 08:48:50 +0000

    fix: Switch to AbstractUser with manager, migration, serializer

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,29 +1,29 @@
 import uuid
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+
 from .managers import CustomUserManager
 
+
 # Create your models here.
-class User(AbstractBaseUser):
+class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.EmailField(unique=True, error_messages={
-            'unique': 'A user with this email already exists'
-        })
-    username = models.CharField(
-            max_length=150,
-            unique=True,
-            blank=True,
-            default=""
-        )
+    email = models.EmailField(
+        unique=True, error_messages={"unique": "A user with this email already exists"}
+    )
+    username = models.CharField(max_length=150, unique=True, blank=True, default="")
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
     class Meta:
-        ordering = ['-date_joined']
+        ordering = ["-date_joined"]
 
     def __str__(self):
         return self.email
+
     def save(self, *args, **kwargs):
         if self.email:
             self.username = self.email.lower()
