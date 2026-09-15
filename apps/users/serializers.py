10c5d3527a6bfd6e2303f7c5f393511c250fd5fc
Faith Etornam <faithgbadegbe1@gmail.com>
2026-09-15 09:51:30 +0000

    fix: Made some alterations on the User model for frontend

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,1 +1,15 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["firstName", "lastName", "otherName", "password", "agreeToTerms"]
+
+    def validate_agreeToTerms(self, value):
+        if not value:
+            raise serializers.ValidationError("You must agree to the terms to register")
+        return value
